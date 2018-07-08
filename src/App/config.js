7bdd6ed2a4875b409dc33e31d33fb641d5bf10be
jhastings <nvrnight@gmail.com>
2018-07-07 21:08:35 -0600

    Added in memory database configuration.

--- src/App/config.js
+++ src/App/config.js
@@ -1,3 +1,19 @@
-module.exports = {
-    storage: 'inmemory'
+let config = {
+    storage: 'files'
 };
+
+let keys = ['storage']
+
+for(var i in process.argv) {
+    let arg = process.argv[i];
+    if(arg.includes("--")) {
+        let argSplit = arg.substring(2).split('=');
+        let key = argSplit[0];
+        let value = argSplit[1];
+        if(keys.includes(key)) {
+            config[key] = value;
+        }
+    }
+}
+
+module.exports = config;
